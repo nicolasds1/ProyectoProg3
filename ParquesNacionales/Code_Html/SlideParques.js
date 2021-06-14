1d28af1d2420bd4a28fe2bc07c7c62346ed14e61
Cristian De Pietro <cris-ar@live.com.ar>
2021-06-14 17:13:41 -0300

    Fix Slides

---
+++ ParquesNacionales/Code_Html/SlideParques.js
@@ -0,0 +1,26 @@
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+// Javascript for image slider manual navigation
+var manualNav = function (manual) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+    });
+});
+
+
