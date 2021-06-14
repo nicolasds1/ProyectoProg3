037a314e25b718782d373d0a420d96265c496d43
Cristian De Pietro <cris-ar@live.com.ar>
2021-06-14 16:26:49 -0300

    Agregado Spring y cambio de directorio de todas las URL

---
+++ ParquesNacionales/src/main/java/com/Parks/ParquesNacionales/ParquesNacionalesApplication.java
@@ -0,0 +1,13 @@
+package com.Parks.ParquesNacionales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParquesNacionalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParquesNacionalesApplication.class, args);
+	}
+
+}
